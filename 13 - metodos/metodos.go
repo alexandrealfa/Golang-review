105703d411280867349b990e5edebe0318399386
Alexandre Alfa <henriqueafcarvalho15@gmail.com>
2022-03-13 22:54:50 -0400

    (master) Implements atualizarIdade method to usuario struct

--- 13 - metodos/metodos.go
+++ 13 - metodos/metodos.go
@@ -7,7 +7,10 @@ type usuario struct {
 	idade uint8
 }
 
-// métodos, são ações de um struct, e outro tipo de dado em go, com ele é possível acessar os dados do mesmo, e realizar modificações a partir do mesmo.
+/*
+ métodos, são ações de um struct, e outro tipo de dado em go, com ele é possível acessar os dados do mesmo,
+ 	e realizar modificações a partir do mesmo.
+*/
 
 func (u usuario) salvar() {
 	fmt.Printf("salvando os dados do usuário %s no banco de dados.\n", u.nome)
@@ -32,6 +35,10 @@ func (u usuario) maiorDeIdade() bool {
 	return false
 }
 
+func (u *usuario) atualizarIdade(novaIdade uint8) {
+	u.idade = novaIdade
+}
+
 func main() {
 	var newUser usuario = usuario{nome: "Alexandre", idade: 23}
 	newUser.salvar()
@@ -45,4 +52,10 @@ func main() {
 	newUser2.fazerAniversario()
 
 	fmt.Println(newUser.idade)
+
+	fmt.Println(newUser2.idade)
+
+	newUser2.atualizarIdade(45)
+
+	fmt.Println("idade Usuario 2: ", newUser2.idade)
 }
