e56f1b17b9db3db3cd3422a6c890845736116855
Alexandre Alfa <henriqueafcarvalho15@gmail.com>
2022-03-16 00:00:53 -0400

    (master) implements Generate Function to accept first command

--- 15 -app-linha-comando/app/app.go
+++ 15 -app-linha-comando/app/app.go
@@ -1,12 +1,42 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"github.com/urfave/cli"
+	"log"
+	"net"
+)
 
 // Generate irá retornar a aplicação de linha de comando pronta para ser executada.
 func Generate() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Aplicação de linha de comando"
 	app.Usage = "Busca Ips e nomes de servidores na internet"
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Busca Ips de endereços na internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "devbook.com.br",
+				},
+			},
+			Action: buscarIps,
+		},
+	}
 
 	return app
+}
+
+func buscarIps(c *cli.Context) {
+	host := c.String("host") //c.Command.Flags.value
+	ips, err := net.LookupIP(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
