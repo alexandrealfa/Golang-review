5fe3eaafd693def5e564368c40191e1ab992d3e2
Alexandre Alfa <henriqueafcarvalho15@gmail.com>
2022-03-17 16:05:11 -0400

    (master) implements searchServers command in app.go

--- 15 -app-linha-comando/app/app.go
+++ 15 -app-linha-comando/app/app.go
@@ -19,17 +19,26 @@ func Generate() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Aplicação de linha de comando"
 	app.Usage = "Busca Ips e nomes de servidores na internet"
+
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "devbook.com.br",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Busca Ips de endereços na internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "devbook.com.br",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Busca Ips de endereços na internet",
+			Flags:  flags,
 			Action: searchIps,
+		},
+		{
+			Name:   "servidores",
+			Usage:  "Busca o Nome dos servidores na internet",
+			Flags:  flags,
+			Action: searchServers,
 		},
 	}
 
@@ -47,5 +56,19 @@ func searchIps(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+// SearchIps é uma função que captura os dados de host do cli, e lista todos os Servidores contidos nele.
+func searchServers(c *cli.Context) {
+	host := c.String("host")
+	servers, err := net.LookupNS(host)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, server := range servers {
+		fmt.Println(server.Host)
 	}
 }
