f02bc77f0ac0d50d617043e24bf570669ac8d4aa
Alexandre Alfa <henriqueafcarvalho15@gmail.com>
2022-03-23 23:03:52 -0400

    (master) Implements Comments about Multiplexador pattern in golang

--- 16 -  Concorrencia/padroes-de-concorrencia/Multiplexador.go
+++ 16 -  Concorrencia/padroes-de-concorrencia/Multiplexador.go
@@ -5,17 +5,23 @@ import (
 	"time"
 )
 
+/*
+	Multiplexador: recebe N canais por parametros e retorna um único canal que filtra os dados recebidos
+	por ambos os canais, e compila tudo em um único canal de retorno, é excelente para tratamento de varios canais e
+	agrupando o retorno de multiplos canais tratados em um único canal.
+*/
+
 func main() {
 	canal, canal2 := writing("Ola Mundo"), writing("Ola Novo Channel")
 
-	canalCompilado := multiplexar(canal, canal2)
-
-	// canalCompilado := poderia ser assim também.
+	canalCompilado := multiplexar(canal, canal2) // canalCompilado := poderia ser assim também.
 
 	for i := 0; i < 10; i++ {
 		fmt.Println(<-canalCompilado)
 	}
 }
+
+// Recebe dois canas e retornar um único canal reponsável por tratar o recebimento dos canais recebidos.
 
 func multiplexar(canalEntrada1, canalEntrada2 <-chan string) <-chan string {
 	canalDeSaida := make(chan string)
