e9899bd3f121f53e3e85bcce446abc2342c10270
Alexandre Alfa <henriqueafcarvalho15@gmail.com>
2022-03-24 23:48:46 -0400

    (master) Implements Unit Test to Enderecos Package.

---
+++ 17 - testes automatizados/enderecos/enderecos_test.go
@@ -0,0 +1,28 @@
+package enderecos
+
+import "testing"
+
+/*
+ => Unit Tests:
+ O nome da função de test é declarada com a inicial Test e na sequência o nome da função que será testada, em camelCase.
+ A função de test recebe o ponteiro de testing, que é importado do repositório de funções go.
+*/
+
+func TestTipoDeEndereco(t *testing.T) {
+	addressToTest := "Avenida Paulista"
+
+	// Definindo o tipo de endereco esperado
+	addressTypeExpected := "avenida"
+
+	// Definindo o tipo de endereco recebido, ao executar a função a ser testada.
+	addressTypeReceived := TipoDeEndereco(addressToTest)
+
+	/* Faz a validação se o valor recebido pela função a ser testada, é diferente do retorno esperado pela mesma
+	caso sejam diferentes aponta ele reprova o test, caso contrario o test passa.
+
+	Para rodar o test basta rodar o comando ```go test``` no terminal.
+	*/
+	if addressTypeReceived != addressTypeExpected {
+		t.Error("O tipo de endereço recebido é diferente do tipo de endereço esperado!.")
+	}
+}
