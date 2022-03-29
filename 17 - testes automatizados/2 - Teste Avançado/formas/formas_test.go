7e4093f7aa76890de736f6a1480a6e6b6ea61f90
Alexandre Alfa <henriqueafcarvalho15@gmail.com>
2022-03-28 23:52:04 -0400

    (master) Implements tests to interfaces in formas_test.go

--- 17 - testes automatizados/2 - Teste Avançado/formas/formas_test.go
+++ 17 - testes automatizados/2 - Teste Avançado/formas/formas_test.go
@@ -1,1 +1,28 @@
 package formas
+
+import (
+	"math"
+	"testing"
+)
+
+func TestArea(t *testing.T) {
+	t.Run("Rectangle", func(t *testing.T) {
+		ret := Rectangle{10, 12}
+		expectedArea := float64(120)
+		receivedArea := ret.area()
+
+		if expectedArea != receivedArea {
+			t.Fatalf("o valor recebido não é compativel com o valor recebido, Recebido : %f, esperado: %f", receivedArea, expectedArea)
+		}
+	})
+
+	t.Run("Circle", func(t *testing.T) {
+		circle := Circle{10}
+		expectedArea := math.Pi * 100
+		receivedArea := circle.area()
+
+		if expectedArea != receivedArea {
+			t.Fatalf("A area recebida: %f, é diferente da área esperada: %f", receivedArea, expectedArea)
+		}
+	})
+}
