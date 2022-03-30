50e6c0dcb22fa42c6d88ad345edc13a5d4da99e2
Alexandre Alfa <henriqueafcarvalho15@gmail.com>
2022-03-30 00:02:10 -0400

    (master) implements content about http

---
+++ 19 - HTTP/http.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+/*
+	função declarada para ser utilizada em uma route, onde por padrão recebe dois parametros,
+	w http.ResponseWriter, dados da response, http.Request payload da requisição
+*/
+
+func home(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(200)
+	_, err := w.Write([]byte("get router"))
+
+	if err != nil {
+		return
+	}
+
+}
+
+func main() {
+
+	/*
+		http.HandleFunc é responsável por definir a uri e a função a ser executada nessa uri,
+		onde a função tem 2 params obrigatórios para tratamento dos dados recebidos.
+	*/
+
+	http.HandleFunc("/home", home)
+
+	/*
+		log.Fatal(http.ListenAndServe(":port", null) é reponsável por gerar
+		um server que atende pela porta definida, para receber requisições.
+	*/
+
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
